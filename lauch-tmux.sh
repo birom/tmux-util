5995a380bd181a5cfd1e510684b3b95699889795
Marton Biro <marton.biro@mimacom.com>
2018-07-14 14:47:05 +0200

    initial commit

---
+++ lauch-tmux.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ENVIRONMENT=$1
+
+. tmux_config-$ENVIRONMENT.properties
+
+tmux new-session -d -s $ENVIRONMENT
+
+IFS=','; first_window=true
+for window in ${windows[@]};
+do
+	if $first_window ; then
+		tmux rename-window $window
+	else
+		tmux new-window -n $window
+	fi
+		
+	if [[ -v $window ]]; then
+		tmux send-keys -t $ENVIRONMENT:$window ${!window} Enter
+	fi
+
+	first_window=false
+done
+
+tmux select-window -t 0
+tmux a -t $ENVIRONMENT
