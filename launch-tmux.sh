8273bea034daed3b62a006227e8712a1f75abd5e
Marton Biro <marton.biro@mimacom.com>
2018-07-18 15:00:30 +0200

    add allow-rename off config

--- launch-tmux.sh
+++ launch-tmux.sh
@@ -5,22 +5,24 @@ ENVIRONMENT=$1
 . tmux_config-$ENVIRONMENT.properties
 
 tmux new-session -d -s $ENVIRONMENT
+tmux set-option -g allow-rename off
 
 IFS=','; first_window=true
 for window in ${windows[@]};
 do
-	if $first_window ; then
-		tmux rename-window $window
-	else
-		tmux new-window -n $window
-	fi
-		
-	if [[ -v $window ]]; then
-		tmux send-keys -t $ENVIRONMENT:$window ${!window} Enter
-	fi
+        if $first_window ; then
+                tmux rename-window $window
+        else
+                tmux new-window -n $window
+        fi
 
-	first_window=false
+        if [[ -v $window ]]; then
+                tmux send-keys -t $ENVIRONMENT:$window ${!window} Enter
+        fi
+
+        first_window=false
 done
 
 tmux select-window -t 0
 tmux a -t $ENVIRONMENT
+
